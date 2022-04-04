79443eff18ed83130d11e0b878a9fdb6463cd78a
Aaron LaBeau <alabeau@gmail.com>
2022-04-04 14:59:39 -0500

    updated for Capella config settings

--- src/Org.Quickstart.API/Models/CouchbaseConfig.cs
+++ src/Org.Quickstart.API/Models/CouchbaseConfig.cs
@@ -9,6 +9,8 @@ namespace Org.Quickstart.API.Models
 	    public string RestEndpoint { get; set; }
 
 		public bool IgnoreRemoteCertificateNameMismatch { get; set; }
+		public bool HttpIgnoreRemoteCertificateMismatch { get; set; }
+		public bool KvIgnoreRemoteCertificateNameMismatch { get; set; }
 
 		public string ConnectionString { get; set; }	
 	    public string Username { get; set; }
